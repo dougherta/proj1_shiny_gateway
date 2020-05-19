61005d25d05c586c99476bdf2fb1ef2076a7214c
Alison Dougherty <alison.p.dougherty@gmail.com>
2020-05-19 13:41:55 -0400

    Adding completed shiny server and ui

--- gateway_shiny/server.R
+++ gateway_shiny/server.R
@@ -2,109 +2,156 @@
 
 function(input, output) {
 
+    
+    # Reactive expression that generates graphs comparing history of trying cigarettes to trying marijuana
     marijF <- reactive({
         req(input$wave)
         
         if(input$wave == "H1")
-            marij <- add_data %>% filter(H1GI8_RACE==input$race, BIO_SEX=='Female') %>% 
+            marij <- add_data %>% filter(H1TO1_TRYcig=='Yes') %>% 
                 ggplot(aes(x = H1TO30_TRYmj)) +
-                labs(title = "Female, Wave I Baseline: Age first tried marijuana", subtitle = "") 
+                labs(title = "Baseline: Yes, tried cigarettes and Age first tried marijuana") 
         else
             if(input$wave == "H2")
-                marij <- add_data %>% filter(H1GI8_RACE==input$race, BIO_SEX=='Female') %>% 
+                marij <- add_data %>% filter(H2TO1=='Yes') %>% 
                     ggplot(aes(x = H1TO30_TRYmj, fill = H2TO44)) +
-                    labs(title = "Female, Age first tried marijuana. Compared to Wave I: \n\tUsed marijuana since last interview?\n\n", fill = "") 
-            else
-                if(input$wave == "H3")
-                    marij <- add_data %>% filter(H1GI8_RACE==input$race, BIO_SEX=='Female') %>% 
-                        ggplot(aes(x = H1TO30_TRYmj, fill = H3TO108)) +
-                        labs(title = "Female, Age first tried marijuana. Compared to Wave I: \nUsed marijuana since June, 1995?\n\n", fill = "") 
-                else 
-                    if(input$wave == "H4")
-                        marij <- add_data %>% filter(H1GI8_RACE==input$race, BIO_SEX=='Female') %>% 
-                            ggplot(aes(x = H1TO30_TRYmj, fill = H4TO65B)) +
-                            labs(title = "Female, Age first tried marijuana. Compared to Wave I: \nEver used marijuana?\n\n", fill = "") 
-                    
+                    labs(title = "Tried cigarettes since last interview. Age first tried marijuana. Compared to Wave I: \n\tUsed marijuana since last interview?\n\n", fill = "") 
+        else
+            if(input$wave == "H3")
+                marij <- add_data %>% filter(H3TO1=='Yes') %>% 
+                    ggplot(aes(x = H1TO30_TRYmj, fill = H3TO108)) +
+                    labs(title = "Have tried cigarettes. Age first tried marijuana. Compared to Wave I: \nUsed marijuana since June, 1995?\n\n", fill = "") 
+        else 
+            if(input$wave == "H4")
+                marij <- add_data %>% filter(H4TO1=='Yes') %>% 
+                    ggplot(aes(x = H1TO30_TRYmj, fill = H4TO65B)) +
+                    labs(title = "Have tried cigarettes. Age first tried marijuana. Compared to Wave I: \nEver used marijuana?\n\n", fill = "") 
     })
     
-    
+    # Reactive expression that generates graphs comparing history of NOT trying cigarettes to trying marijuana
     marijM <- reactive({
         req(input$wave)
         
             if(input$wave == "H1")
-                marij <- add_data %>% filter(H1GI8_RACE==input$race, BIO_SEX=='Male') %>% 
+                marij <- add_data %>% filter(H1TO1_TRYcig=="No (skip to Q.9)") %>% 
                     ggplot(aes(x = H1TO30_TRYmj)) +
-                    labs(title = "Male, Wave I Baseline: Age first tried marijuana", subtitle = "") 
+                    labs(title = "Baseline: Never tried cigarettes and Age first tried marijuana", subtitle = "") 
             else
                 if(input$wave == "H2")
-                    marij <- add_data %>% filter(H1GI8_RACE==input$race, BIO_SEX=='Male') %>% 
+                    marij <- add_data %>% filter(H2TO1=="No (skip to Q9)") %>% 
                         ggplot(aes(x = H1TO30_TRYmj, fill = H2TO44)) +
-                        labs(title = "Male, Age first tried marijuana. Compared to Wave I: \n\tUsed marijuana since last interview?\n\n", fill = "") 
+                        labs(title = "Haven't tried cigarette since last interview. Age first tried marijuana compared to Wave I: \n\tUsed marijuana since last interview?\n\n", fill = "") 
             else
                 if(input$wave == "H3")
-                    marij <- add_data %>% filter(H1GI8_RACE==input$race, BIO_SEX=='Male') %>% 
+                    marij <- add_data %>% filter(H3TO1=="No (skip to Q.27)") %>% 
                         ggplot(aes(x = H1TO30_TRYmj, fill = H3TO108)) +
-                        labs(title = "Male, Age first tried marijuana. Compared to Wave I: \nUsed marijuana since June, 1995?\n\n", fill = "") 
+                        labs(title = "Never tried cigarettes and Age first tried marijuana compared to Wave I: \nUsed marijuana since June, 1995?\n\n", fill = "") 
             else 
                 if(input$wave == "H4")
-                    marij <- add_data %>% filter(H1GI8_RACE==input$race, BIO_SEX=='Male') %>% 
+                    marij <- add_data %>% filter(H4TO1=='No') %>% 
                         ggplot(aes(x = H1TO30_TRYmj, fill = H4TO65B)) +
-                        labs(title = "Male, Age first tried marijuana. Compared to Wave I: \nEver used marijuana?\n\n", fill = "") 
-        
+                        labs(title = "Never tried cigarettes and Age first tried marijuana compared to Wave I: \nEver used marijuana?\n\n", fill = "") 
     })
 
 
-    # marijuana
-
+    # marijuana 
     output$countF <- renderPlotly({
         
         p <- marijF()
         
         p <- p + 
-            # ggtitle("Self-report: Age for trying marijuana") +
             xlab("Age") + ylab("Count") +
             geom_bar() + coord_flip() +
             scale_fill_brewer(palette = "Accent") + 
             theme_bw()
-        
-        
     })
-    
-    
-        output$countM <- renderPlotly({
+
+
+    output$countM <- renderPlotly({
 
         p <- marijM()
             
         p <- p + 
-            # ggtitle("Self-report: Age for trying marijuana") +
             xlab("Age") + ylab("Count") +
             geom_bar() + coord_flip() +
             scale_fill_brewer(palette = "Dark2") + 
             theme_bw()
-        
-
     })
+
+########################################################################################################################
+    # Reactive expression that generates graphs comparing history of trying cigarettes to trying cocaine
+    cocaY <- reactive({
+        req(input$wave)
+        
+        if(input$wave == "H1")
+            add_data %>% filter(H1TO1_TRYcig=='Yes') %>% 
+                ggplot(aes(x = H1TO34_TRYcoc)) +
+                labs(title = "Baseline: Yes, tried cigarettes and Age first tried cocaine") 
+        else
+            if(input$wave == "H2")
+                add_data %>% filter(H2TO1=='Yes') %>% 
+                    ggplot(aes(x = H1TO34_TRYcoc, fill = H2TO50)) +
+                    labs(title = "Tried cigarettes since last interview. Age first tried cocaine. Compared to Wave I: \n\tUsed cocaine since last interview?\n\n", fill = "") 
+            else
+                if(input$wave == "H3")
+                    add_data %>% filter(H3TO1=='Yes') %>% 
+                        ggplot(aes(x = H1TO34_TRYcoc, fill = H3TO111)) +
+                        labs(title = "Have tried cigarettes. Age first tried cocaine. Compared to Wave I: \nUsed cocaine since June, 1995?\n\n", fill = "") 
+                else 
+                    if(input$wave == "H4")
+                        add_data %>% filter(H4TO1=='Yes') %>% 
+                            ggplot(aes(x = H1TO34_TRYcoc, fill = H4TO65C)) +
+                            labs(title = "Have tried cigarettes. Age first tried cocaine. Compared to Wave I: \nEver used cocaine?\n\n", fill = "") 
+    })
+    
+    # Reactive expression that generates graphs comparing history of NOT trying cigarettes to trying cocaine
+    cocaN <- reactive({
+        req(input$wave)
+        
+        if(input$wave == "H1")
+            add_data %>% filter(H1TO1_TRYcig=="No (skip to Q.9)") %>% 
+                ggplot(aes(x = H1TO34_TRYcoc)) +
+                labs(title = "Baseline: Never tried cigarettes and Age first tried cocaine", subtitle = "") 
+        else
+            if(input$wave == "H2")
+                add_data %>% filter(H2TO1=="No (skip to Q9)") %>% 
+                    ggplot(aes(x = H1TO34_TRYcoc, fill = H2TO50)) +
+                    labs(title = "Haven't tried cigarette since last interview. Age first tried cocaine compared to Wave I: \n\tUsed cocaine since last interview?\n\n", fill = "") 
+            else
+                if(input$wave == "H3")
+                    add_data %>% filter(H3TO1=="No (skip to Q.27)") %>% 
+                        ggplot(aes(x = H1TO34_TRYcoc, fill = H3TO111)) +
+                        labs(title = "Never tried cigarettes and Age first tried cocaine compared to Wave I: \nUsed cocaine since June, 1995?\n\n", fill = "") 
+                else 
+                    if(input$wave == "H4")
+                        add_data %>% filter(H4TO1=='No') %>% 
+                            ggplot(aes(x = H1TO34_TRYcoc, fill = H4TO65C)) +
+                            labs(title = "Never tried cigarettes and Age first tried cocaine compared to Wave I: \nEver used cocaine?\n\n", fill = "") 
+    })
+    
+    
+    # cocaine
+    output$countCY <- renderPlotly({
+        
+        p <- cocaY()
+        
+        p <- p + 
+            xlab("Age") + ylab("Count") +
+            geom_bar() + coord_flip() +
+            scale_fill_brewer(palette = "Accent") + 
+            theme_bw()
+    })
+    
+    
+    output$countCN <- renderPlotly({
+        
+        p <- cocaN()
+        
+        p <- p + 
+            xlab("Age") + ylab("Count") +
+            geom_bar() + coord_flip() +
+            scale_fill_brewer(palette = "Dark2") + 
+            theme_bw()
+    })    
 }
 
-
-# function(input, output) {
-# 
-#     output$count <- renderPlotly(
-# 
-#         add_data %>%
-#             filter(BIO_SEX == input$bio_sex & H1GI8_RACE == input$race) %>%
-# 
-#             ggplot(aes(x = H1TO30_TRYmj)) +
-#             ggtitle("Self-report: Age for trying marijuana") +
-#             xlab("Age") +
-#             ylab("Count") +
-# 
-#             geom_bar(fill="steelblue") + coord_flip() +
-#             theme_bw()
-# 
-#     )
-# }
-
-# {if (input$bio_sex==T) dplyr::group_by(., BIO_SEX) else .} %>% 
-# {if (input$bio_sex==F) dplyr::group_by(.) else .} %>% 
-    
--- gateway_shiny/ui.R
+++ gateway_shiny/ui.R
@@ -1,7 +1,9 @@
 
 
 fluidPage(theme = shinytheme("flatly"),
-        titlePanel("Gateways: Case Studies"),
+        titlePanel("Gateway Theory: Excerpt from the National Longitudinal Study 
+                   of Adolescent to Adult Health (Add Health), 1994-2008"),
+        hr(),
             
         
         sidebarLayout(
@@ -16,32 +18,65 @@ fluidPage(theme = shinytheme("flatly"),
                                             "Wave III: 2001-02" = "H3",
                                             "Wave IV: 2007-09" = "H4"),
                              selected = "H1"),
-
-                
-                # checkboxInput(inputId = "bio_sex", label = "Group by sex", value = TRUE),
-                # selectizeInput(inputId = "bio_sex",
-                #                label = "Sex",
-                #                choices = levels(add_data$BIO_SEX)),
-                
-                selectizeInput(inputId = "race",
-                               label = "Race",
-                               choices = levels(add_data$H1GI8_RACE),
-                               selected = "Other"),
-                
-                # selectizeInput(inputId = "yob",
-                #                label = "Year of birth",
-                #                choices = sort(unique(add_data$H1GI1Y)),
-                #                selected = "")
-                
-                
             ),
             
             mainPanel(
                     
-                    plotlyOutput("countF"),
-                    
-                    plotlyOutput("countM")
-                    
+                    tabsetPanel(type = "tabs",
+                                
+                                tabPanel("General Information",
+                                         h4("What is the Gateway Theory?"),
+                                         p("The gateway theory proposes that early experimentation with tobacco or alcohol escalates to more addictive
+                                           illicit substances later in adulthood. This theory has been studied consistently since the 1970s with 
+                                           more or less confirmation depending on the source. Regardless of the level of support achieved by investigators, 
+                                           societally the theory has been generally accepted and influenced much of the public health policy instituted 
+                                           in the last several decades."),
+                                         h4("Data source for this Shiny application"),
+                                         p("This Shiny application excerpts from the National Longitudinal Study of Adolescent to Adult Health (Add Health). 
+                                           The Add Health study is a national longitudinal survey of representative sample of students in grades 7-12 in the 1994 
+                                           school year in the United States. This sample has been followed with three further in-home interviews in 1996 Wave II 
+                                           (11-21 years), 2001-2002 Wave III (aged 18-26 years), and 2007-2008 Wave IV (aged 24-32 years). This excerpt used the 
+                                           restricted Add Health datasets as downloaded from the ICPSR Institute of Social Research, hosted by the University of 
+                                           Michigan."),
+                                         h4("How to use this app"),
+                                         p("The analysis shown within the two tabs above, labeled Marijuana and Cocaine, demonstrate the relationship
+                                           over time of experimentation with tobacco cigarettes and marijuana or cocaine. The sidepanel shows which Wave of data 
+                                           is being visualized. As you click through the waves you will see the overlay of new responses to having tried marijuana 
+                                           or cocaine as compared to the individual's first response at their Baseline interview in 1995.")
+                                         ),
+                                
+                                tabPanel("Marijuana", 
+                                         br(),
+                                         p("These graphs convey the correlation between individuals who have or have not \"tried\" 
+                                           cigarette(s) across the waves of the study with those who have tried marijuana."),
+                                         p("The top graph is only individuals who answered Yes to having tried cigarettes during 
+                                           the time period before or between each wave of the study."),
+                                         p("The bottom graph is individuals who answered No to having tried cigarettes during 
+                                           the time period before or between each wave of the study."),
+                                         em("Please note: In neither case do these results cover whether or not these individuals 
+                                            became regular users of either cigarettes or marijuana. "),
+                                         br(),
+                                         
+                                         plotlyOutput("countF"),
+                                         br(),
+                                         plotlyOutput("countM")),
+                                
+                                tabPanel("Cocaine",
+                                         br(),
+                                         p("These graphs convey the correlation between individuals who have or have not \"tried\" 
+                                           cigarette(s) across the waves of the study with those who have tried cocaine."),
+                                         p("The top graph is only individuals who answered Yes to having tried cigarettes during 
+                                           the time period before or between each wave of the study."),
+                                         p("The bottom graph is individuals who answered No to having tried cigarettes during 
+                                           the time period before or between each wave of the study."),
+                                         em("Please note: In neither case do these results cover whether or not these individuals 
+                                            became regular users of either cigarettes or cocaine. "),
+                                         br(),
+                                         
+                                         plotlyOutput("countCY"),
+                                         br(),
+                                         plotlyOutput('countCN'))
+                    )
                 )
         )
 )
